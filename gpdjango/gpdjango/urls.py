c90e2b60c54781eafeb72856854202afd039d732
rau <max.rauchaplin@gmail.com>
2014-09-25 10:01:39 -0300

    Created first view(page) at localhost:8000/gpcore

--- gpdjango/gpdjango/urls.py
+++ gpdjango/gpdjango/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'gpdjango.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^gpcore/', include('gpcore.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
